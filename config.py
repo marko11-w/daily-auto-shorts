80519aad0e1b2d0e0fa6a0f1f498bac6edebde13
marko11-w <69992888+marko11-w@users.noreply.github.com>
2025-11-13 12:55:20 +0300

    Update and rename config.py to users.json

--- config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8321031169:AAHd7BIrJrNr3bR44wj7HBO8ADwXLXGANKA")
-OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
-ADMIN_ID = int(os.environ.get("ADMIN_ID", "7758666677"))
